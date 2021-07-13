40942c12da4eea8f02d251cf81e7be1c1dc16ce6
Raj Shubham <sraj17297@gmail.com>
2021-07-13 22:54:19 +0530

    Adding 2 new projects

--- Portfolio2.js
+++ Portfolio2.js
@@ -1,102 +1,100 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-            $('.social-ul a').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-            $('.social-ul a').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+      $(".social-ul a").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+      $(".social-ul a").removeClass("sticky");
+    }
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
 
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+  $(".navbar .menu li a").click(function () {
+    // applying again smooth scroll on menu items click
+    $("html").css("scrollBehavior", "smooth");
+  });
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Data-Science Enthusiast"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 
-    var typed = new Typed(".typing-2", {
-        strings:["Web Developer", "Data-Science Enthusiast"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: ["App Developer", "Full Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
+  var typed = new Typed(".typing-2", {
+    strings: ["App Developer", "Full Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 
-    var modalBtn = document.querySelector('.modal-btn');
-      var modalBg = document.querySelector('.modal-bg');
-      var modalClose = document.querySelector('.modal-close');
+  // owl carousel script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
 
-      modalBtn.addEventListener('click',function(){
-          modalBg.classList.add('bg-active');
-      });
-      modalClose.addEventListener('click',function(){
-        modalBg.classList.remove('bg-active');
-      });
+  var modalBtn = document.querySelector(".modal-btn");
+  var modalBg = document.querySelector(".modal-bg");
+  var modalClose = document.querySelector(".modal-close");
 
+  modalBtn.addEventListener("click", function () {
+    modalBg.classList.add("bg-active");
+  });
+  modalClose.addEventListener("click", function () {
+    modalBg.classList.remove("bg-active");
+  });
 
-      $(".animated-progress span").each(function () {
-        $(this).animate(
-          {
-            width: $(this).attr("data-progress") + "%",
-          },
-          1000
-        );
-        $(this).text($(this).attr("data-progress") + "%");
-      });
-
+  $(".animated-progress span").each(function () {
+    $(this).animate(
+      {
+        width: $(this).attr("data-progress") + "%",
+      },
+      1000
+    );
+    $(this).text($(this).attr("data-progress") + "%");
+  });
 });
 
-    var preloader = document.getElementById('loading');
-     function loader(){
-         preloader.style.display = 'none';
-     }
+var preloader = document.getElementById("loading");
+function loader() {
+  preloader.style.display = "none";
+}
